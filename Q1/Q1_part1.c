8cacd12bffa33cd2466a0228db0433b90e4a1238
yoshinnarang <55681130+yoshinnarang@users.noreply.github.com>
2020-11-11 21:57:17 +0530

    Update Q1_part1.c

--- Q1/Q1_part1.c
+++ Q1/Q1_part1.c
@@ -1,2 +1,34 @@
-/* Name: Your Name
-   Roll_Number: Your Roll_Number */
+ Name: Yoshin Narang
+   Roll_Number: 2019221
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int i = 10;
+
+int main()
+{
+    pid_t process = fork();
+    if (process>0){
+        // printf("%s","Yoshin");
+    while (i <= 100)
+    {
+        
+        printf("pid = %d, i = %d\n", getpid(), i);
+        i++;
+    }
+    }
+    else{
+    while(i>=-90){
+         
+        printf("pid = %d, i = %d\n", getpid(), i);
+        i--;
+
+    }
+    }
+    
+    
+    return 0;
+}
