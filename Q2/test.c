fb7f5ff8fcb20031525a35f64dd9c22dc3e2711f
yoshinnarang <55681130+yoshinnarang@users.noreply.github.com>
2020-11-12 15:59:08 +0530

    Update test.c

--- Q2/test.c
+++ Q2/test.c
@@ -1,2 +1,20 @@
-/* Name: Your Name
-   Roll_Number: Your Roll_Number */
+ Name: Yoshin Narang
+   Roll_Number: 2019221
+      
+      
+#define SYS_shtaskinfo 440
+
+
+int main(void)
+{
+  // if (argc <= 1) {
+  //   printf("Must provide a string to give to system call.\n");
+  //   return -1;
+  // }
+  // char *arg = argv[1];
+  // printf("Making system call with \"%s\".\n", arg);
+  // int res = syscall(440);
+  int res = syscall(SYS_shtaskinfo(fileno,549));
+  printf("%d\n",res);
+  
+      
